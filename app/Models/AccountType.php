5349b5d90a819c03b7ad09fb5d2c38756be1aa58
Rafael Castillo <rafaelcastillo@outlook.com>
2020-09-23 21:30:02 -0500

    AccountType model+table+seeder

---
+++ app/Models/AccountType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AccountType extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'default_days_authorization_valid'
+    ];
+
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\AccountType::create([ 'name' => 'Alumno', 'default_days_authorization_valid' => 1]);
+        \App\Models\AccountType::create([ 'name' => 'Profesor', 'default_days_authorization_valid' => 7]);
+        \App\Models\AccountType::create([ 'name' => 'Administrativo', 'default_days_authorization_valid' => 7]);
     }
 }
