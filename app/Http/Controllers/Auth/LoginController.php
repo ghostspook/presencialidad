60dd52997a590980fee4363bf386106413fcb9f5
Rafael Castillo <rafaelcastillo@outlook.com>
2020-09-23 19:36:45 -0500

    Authenticates with Google

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider()
+    {
+        return Socialite::driver('google')
+            ->with(['hd' => 'ide.edu.ec'])
+            ->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback()
+    {
+        $social_user = Socialite::driver('google')->user();
+
+        if ($user = User::where('email', $social_user->email)->first()) {
+            return $this->authAndRedirect($user); // Login y redirección
+        } else {
+            // En caso de que no exista creamos un nuevo usuario con sus datos.
+            $user = User::create([
+                'name' => $social_user->name,
+                'email' => $social_user->email,
+                'provider' => 'google',
+            ]);
+
+            return $this->authAndRedirect($user); // Login y redirección
+        }
+    }
+
+    public function authAndRedirect($user)
+    {
+        Auth::login($user);
+
+        return redirect()->to('/home');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login/google', [LoginController::class, 'redirectToProvider']);
+Route::get('login/google/callback', [LoginController::class, 'handleProviderCallback']);
+
+Route::get('home', [HomeController::class, 'index']);
