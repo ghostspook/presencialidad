b43366290a7a67080b746610f59768f118a14c9f
Rafael Castillo <rafaelcastillo@outlook.com>
2020-09-26 10:29:44 -0500

    títulos centrados

--- resources/views/advicednottoattend.blade.php
+++ resources/views/advicednottoattend.blade.php
@@ -3,7 +3,7 @@
 @section('main-content')
     <div id="app" class="row">
         <div class="col-md-12">
-            Por su salud se le recomienda no asistir
+            <h1 class="title text-center">Por su salud se le recomienda no asistir</h1>
         </div>
     </div>
 @endsection
