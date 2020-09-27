15ad914d6f3dadf73a0bb1a6bdbcf083520c4993
Rafael Castillo <rafaelcastillo@outlook.com>
2020-09-27 18:08:21 -0500

    Shows expiration date and time

--- resources/views/authorization.blade.php
+++ resources/views/authorization.blade.php
@@ -3,7 +3,17 @@
 @section('main-content')
     <div id="app" class="row">
         <div class="col-md-12">
-            {!! QrCode::size(200)->generate($a->code); !!}
+            <div class="text-center">
+                {!! QrCode::size(200)->generate($a->code); !!}
+                <h1 class="title mt-5">Código QR de Autorización</h1>
+                <p>
+                    Válido hasta el
+                </p>
+                <h2>
+                    {{ $a->expires_at->day }} / {{ $a->expires_at->month }} / {{ $a->expires_at->year }}<br>
+                    {{ $a->expires_at->hour }}:{{ $a->expires_at->minute }}
+                </h2>
+            </div>
         </div>
     </div>
 @endsection
