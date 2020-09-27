da30b92fa7b0e7c01139225db21ec9ccd451a7fd
Rafael Castillo <rafaelcastillo@outlook.com>
2020-09-27 16:36:34 -0500

    Checkboxes

--- resources/views/questionnaireone.blade.php
+++ resources/views/questionnaireone.blade.php
@@ -8,27 +8,81 @@
                 <h1 class="title text-center">Cuestionario habilitante</h1>
                 <h3>¿Tiene Usted y/o algún familiar que viva en su misma residencia (personas con las que entro en contacto directo)?</h3>
                 <div class="ml-4">
-                    <input type="checkbox" name="condition_1" value="Hipertensión arterial"> Hipertensión arterial<br>
-                    <input type="checkbox" name="condition_2" value="Afecciones pulmonares"> Afecciones pulmonares<br>
-                    <input type="checkbox" name="condition_3" value="Insuficiencia renal"> Insuficiencia renal<br>
-                    <input type="checkbox" name="condition_4" value="Lupus"> Lupus<br>
-                    <input type="checkbox" name="condition_5" value="Cáncer"> Cáncer<br>
-                    <input type="checkbox" name="condition_6" value="Diabetes mellitus"> Diabetes mellitus<br>
-                    <input type="checkbox" name="condition_7" value="Obesidad"> Obesidad<br>
-                    <input type="checkbox" name="condition_8" value="Insuficiencia hepática o metabólica"> Insuficiencia hepática o metabólica<br>
-                    <input type="checkbox" name="condition_9" value="Asma"> Asma<br>
-                    <input type="checkbox" name="condition_10" value="Fibromialgia"> Fibromialgia<br>
-                    <input type="checkbox" name="condition_11" value="Trombosis venosa profunda"> Trombosis venosa profunda<br>
-                    <input type="checkbox" name="condition_11" value="Antecedentes de infarto al miocardio"> Antecedentes de infarto al miocardio<br>
-                    <input type="checkbox" name="condition_11" value="Antecedentes de eventos cerebro vasculares"> Antecedentes de eventos cerebro vasculares<br>
-                    <input type="checkbox" name="condition_11" value="Enfermedad cardíaca"> Enfermedad cardíaca<br>
-                    <input type="checkbox" name="condition_11" value="Mujer embarazada"> Mujer embarazada<br>
-                    <input type="checkbox" name="condition_11" value="Mujer en período de lactancia"> Mujer en período de lactancia<br>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="condition_1" name="condition_1" value="Hipertensión arterial">
+                        <label class="custom-control-label" for="condition_1"> Hipertensión arterial</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="condition_2" name="condition_2" value="Hipertensión arterial">
+                        <label class="custom-control-label" for="condition_2"> Afecciones pulmonares</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="condition_3" name="condition_3" value="Hipertensión arterial">
+                        <label class="custom-control-label" for="condition_3"> Insuficiencia renal</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="condition_4" name="condition_4" value="Hipertensión arterial">
+                        <label class="custom-control-label" for="condition_4"> Lupus</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="condition_5" name="condition_5" value="Hipertensión arterial">
+                        <label class="custom-control-label" for="condition_5"> Cáncer</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="condition_6" name="condition_6" value="Hipertensión arterial">
+                        <label class="custom-control-label" for="condition_6"> Diabetes mellitus</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="condition_7" name="condition_7" value="Hipertensión arterial">
+                        <label class="custom-control-label" for="condition_7"> Obesidad</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="condition_8" name="condition_8" value="Hipertensión arterial">
+                        <label class="custom-control-label" for="condition_8"> Insuficiencia hepática o metabólica</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="condition_9" name="condition_9" value="Hipertensión arterial">
+                        <label class="custom-control-label" for="condition_9"> Asma</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="condition_10" name="condition_10" value="Hipertensión arterial">
+                        <label class="custom-control-label" for="condition_10"> Fibromialgia</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="condition_11" name="condition_11" value="Hipertensión arterial">
+                        <label class="custom-control-label" for="condition_11"> Trombosis venosa profunda</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="condition_12" name="condition_12" value="Hipertensión arterial">
+                        <label class="custom-control-label" for="condition_12"> Antecedentes de infarto al miocardio</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="condition_13" name="condition_13" value="Hipertensión arterial">
+                        <label class="custom-control-label" for="condition_13"> Antecedentes de eventos cerebro vasculares</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="condition_14" name="condition_14" value="Hipertensión arterial">
+                        <label class="custom-control-label" for="condition_14"> Enfermedad cardíaca</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="condition_15" name="condition_15" value="Hipertensión arterial">
+                        <label class="custom-control-label" for="condition_15"> Mujer embarazada</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="condition_16" name="condition_16" value="Hipertensión arterial">
+                        <label class="custom-control-label" for="condition_16"> Mujer en período de lactancia</label>
+                    </div>
                 </div>
                 <h3 class="mt-5">Marque si usted convive con uno a varios de los siguientes funcionarios o trabajadores</h3>
                 <div class="ml-4">
-                    <input type="checkbox" name="contact_1" value="Personal de Salud que atienden de manera directa a pacientes con COVID-19"> Personal de Salud que atienden de manera directa a pacientes con COVID-19<br>
-                    <input type="checkbox" name="contact_2" value="Otras profesiones en riesgo por contacto directo con COVID-19"> Otras profesiones en riesgo por contacto directo con COVID-19<br>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="circumstance_1" name="circumstance_1" value="Personal de Salud que atienden de manera directa a pacientes con COVID-19">
+                        <label class="custom-control-label" for="circumstance_1"> Personal de Salud que atienden de manera directa a pacientes con COVID-19</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="circumstance_2" name="circumstance_2" value="Hipertensión arterial">
+                        <label class="custom-control-label" for="circumstance_2"> Otras profesiones en riesgo por contacto directo con COVID-19</label>
+                    </div>
                 </div>
                 <div class="row mt-4">
                      <div class="col-md-12 d-flex justify-content-center">
