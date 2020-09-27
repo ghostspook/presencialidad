5e76fe9a4638d2a5fccbdb158e42317d7f10a548
Rafael Castillo <rafaelcastillo@outlook.com>
2020-09-27 18:19:47 -0500

    Cosmetic

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,12 @@
 @section('main-content')
     <div id="app" class="row">
         <div class="col-md-12">
-            Esta es la página de bienvenida
+            <div class="text-center">
+                <h1 class="title">Bienvenido/a</h1>
+                <h3 class="title">al Sistema de Control para el</h1>
+                <h1 class="title text-primary">Protocolo de retorno a la presencialidad</h1>
+                <a class="btn btn-primary mt-5 btn-lg" href="/login/google" role="button">Iniciar sesión con cuenta &#64;ide.edu.ec</a>
+            </div>
         </div>
     </div>
 @endsection
