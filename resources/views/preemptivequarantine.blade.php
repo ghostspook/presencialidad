7f4c039d81ffedee66937a8a1c2e5a3b7cd244a9
Rafael Castillo <rafaelcastillo@outlook.com>
2020-09-27 23:17:12 -0500

    Cosmético

--- resources/views/preemptivequarantine.blade.php
+++ resources/views/preemptivequarantine.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.app')
 
 @section('main-content')
-    <div id="app" class="row">
-        <div class="col-md-12">
-            Aislamiento preventivo
+<div id="app" class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="/images/warning.png">
         </div>
+        <h2 class="display text-center mt-5">Por su salud y la de todos</h2>
+        <h1 class="display text-center"><span class="text-danger">Le solicitamos no asistir temporalmente de manera presencial.</span></h1>
     </div>
+</div>
 @endsection
