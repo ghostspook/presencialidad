b88eb89be74be280355614c026fe320d39b4ae8d
Rafael Castillo <rafaelcastillo@outlook.com>
2020-09-28 15:31:18 -0500

    Table instead of list

--- resources/views/userspendingtests.blade.php
+++ resources/views/userspendingtests.blade.php
@@ -2,12 +2,22 @@
 
 @section('main-content')
     <div id="app" class="row">
+        <h1 class="table">Pruebas pendientes</h1>
         <div class="col-md-12">
-            <ul>
+            <table class="table">
+                <tr>
+                    <th>Nombre</th>
+                    <th>Email</th>
+                    <th>Estado</th>
+                </tr>
             @foreach ($cards as $card)
-                <li><a href="{{ route('newtestresult', $card->user_id) }}">{{ $card->user->name }}</a> ({{ $card->getStateText() }})</li>
+                <tr>
+                    <td><a href="{{ route('newtestresult', $card->user_id) }}">{{ $card->user->name }}</a></td>
+                    <td>{{ $card->user->email }}</td>
+                    <td>{{ $card->getStateText() }}</td>
+                </tr>
             @endforeach
-            </ul>
+            </table>
         </div>
     </div>
 @endsection
