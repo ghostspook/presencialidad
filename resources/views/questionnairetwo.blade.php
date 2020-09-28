f7bedcb4e73880f11a756e87b14cee2c3c009573
Rafael Castillo <rafaelcastillo@outlook.com>
2020-09-27 20:15:17 -0500

    redacción

--- resources/views/questionnairetwo.blade.php
+++ resources/views/questionnairetwo.blade.php
@@ -36,7 +36,7 @@
                         </label>
                     </div>
                 </div>
-                <h3 class="mt-5">¿Ha estado en <strong>"contacto estrecho"</strong> (a menos de dos metros de distancia, sin mascarilla y por más de 15 minutos) con una persona confirmada por PCR de tener COVID-19? </h3>
+                <h3 class="mt-5">¿Ha estado en <strong>"contacto estrecho"</strong> a menos de dos metros de distancia, con mascarilla y por más de 15 minutos con una persona confirmada por PCR de tener actualmente COVID-19? </h3>
                 <div class="ml-4">
                     <div class="form-check">
                         <input class="form-check-input" type="radio" name="question3" id="r5" value="En contacto con sospechoso COVID-19" checked>
@@ -51,7 +51,7 @@
                         </label>
                     </div>
                 </div>
-                <h3 class="mt-5">¿Usted ha estado en fiestas o reuniones sociales y/o familiares en las que no se ha respetado el distanciamiento y el uso de mascarilla?</h3>
+                <h3 class="mt-5">¿Usted ha estado en fiestas o reuniones sociales y/o familiares en las que considera que ha estado en <strong>"contacto extrecho"</strong>. no se ha respetado el distanciamiento y el uso de mascarilla?</h3>
                 <div class="ml-4">
                     <div class="form-check">
                         <input class="form-check-input" type="radio" name="question4" id="r7" value="Reuniones sociales con riesgo" checked>
