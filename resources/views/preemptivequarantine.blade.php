1736adf5b2296a743c80648696870d4ece7bce28
Rafael Castillo <rafaelcastillo@outlook.com>
2020-09-28 16:55:44 -0500

    msg

--- resources/views/preemptivequarantine.blade.php
+++ resources/views/preemptivequarantine.blade.php
@@ -8,6 +8,7 @@
         </div>
         <h2 class="display text-center mt-5">Por su salud y la de todos</h2>
         <h1 class="display text-center"><span class="text-danger">Le solicitamos no asistir temporalmente de manera presencial.</span></h1>
+        <h1 class="display text-center"><span class="text-danger">Para retornar, deberá subir una prueba PCR con resultado negativo realizada no más allá de un mes.</span></h1>
     </div>
 </div>
 @endsection
