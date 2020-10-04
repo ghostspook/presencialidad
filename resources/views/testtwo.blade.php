e994a05031c8b2d3f26aaafdcecf1febf1905f3e
Rafael Castillo <rafaelcastillo@outlook.com>
2020-10-04 13:02:46 -0500

    Displays last negative-result test date

--- resources/views/testtwo.blade.php
+++ resources/views/testtwo.blade.php
@@ -8,6 +8,15 @@
         </div>
         <h2 class="display text-center mt-5">Por favor, acérquese a Veris a realizarse la</h2>
         <h1 class="display text-center"><strong class="text-success">Siguiente</strong><span class="text-primary"> Prueba Rápida.</span></h1>
+    <p class="text-center mt-5 text-muted">
+        <small>
+            Su última prueba con resultado negativo fue el<br>
+            <strong>
+                {{ Auth::user()->userCard->most_recent_negative_test_result_at->format('d-M-yy') }}
+            </strong>
+        </small>
+
+    </p>
     </div>
 </div>
 @endsection
