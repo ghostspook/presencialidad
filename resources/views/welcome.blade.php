4540640395a6b152b29678c8c7533f98e9aecc08
Rafael Castillo <pcastill@mac.com>
2021-07-21 11:51:50 -0500

    Change text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
             <div class="text-center">
                 <h1 class="title">Bienvenido/a</h1>
                 <h3 class="title">al Sistema de Control para el</h1>
-                <h1 class="title text-primary">Protocolo de retorno a la presencialidad</h1>
+                <h1 class="title text-primary">Protocolo de Presencialidad</h1>
                 <a class="btn btn-primary mt-5 btn-lg" href="/login/google" role="button">Iniciar sesión con cuenta &#64;ide.edu.ec</a>
             </div>
         </div>
